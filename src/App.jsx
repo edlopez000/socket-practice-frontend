360953ed8b5b105a749146b1f245eaf955bbd027
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-05-03 00:36:30 -0400

    Barebones frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://127.0.0.1:4001';
 function App() {
-  return <p>Howdy!</p>;
+  const [res, setRes] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('FromBackend', (data) => {
+      setRes(data);
+    });
+  }, []);
+
+  return <p>{res}</p>;
 }
 
 export default App;
