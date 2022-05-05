1a7dbd86c597dc498bbe814e68021293da32464e
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-05-05 01:06:42 -0400

    App now loads with the correct number of live connections to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-const ENDPOINT = 'http://127.0.0.1:4001';
+import io from 'socket.io-client';
+const ENDPOINT = 'http://localhost:4001';
 function App() {
-  const [res, setRes] = useState('');
+  const [res, setRes] = useState('nothing here yet');
+  const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on('FromBackend', (data) => {
+    const newSocket = io(ENDPOINT);
+    setSocket(newSocket);
+
+    newSocket.on('totalConnections', (data) => {
+      console.log(data);
       setRes(data);
     });
   }, []);
 
-  return <p>{res}</p>;
+  return (
+    <>
+      <p>Total Connections in this session: {res}</p>
+      <br />
+    </>
+  );
 }
 
 export default App;
